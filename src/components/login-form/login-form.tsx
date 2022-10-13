a86ccdb75bca7797c728c87a3d6d8fb01f704985
Oleg <akimba@gmail.com>
2022-10-13 12:26:48 +0300

    feat: Стилизует bg формы логина

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Card } from 'antd';
 import Item from 'antd/lib/form/FormItem';
 import { IUser } from '../../types';
 import { AppDispatch } from '../../store/store';
 import { login } from '../../store/assync-actions';
 import { validate } from '../../utils/common';
 import { ValidateMessage } from '../../const';
-
 
 function LoginForm(): JSX.Element {
   const [user, setUser] = useState<string>('');
@@ -19,51 +18,54 @@ function LoginForm(): JSX.Element {
   const onFinish = () => {
     setIsLoading(true);
     const userData: IUser = { username: user, password };
-    dispatch(
-      login(userData,
-      () => setIsLoading(false)
-    ));
+    dispatch(login(userData, () => setIsLoading(false)));
   };
 
   return (
-    <Form
-      className="form-login"
-      name="login form"
-      labelCol={{ span: 4 }}
-      wrapperCol={{ offset: 1 }}
-      onFinish={onFinish}
-      autoComplete="off"
-    >
-      <Item
-        label="Username"
-        name="username"
-        rules={[validate.required(ValidateMessage.UserName)]}
+    <Card>
+      <Form
+        className="form-login"
+        name="login form"
+        labelCol={{ span: 4 }}
+        wrapperCol={{ offset: 1 }}
+        onFinish={onFinish}
+        autoComplete="off"
       >
-        <Input value={user} onChange={(evt) => setUser(evt.target.value)} placeholder='foo'/>
-      </Item>
-
-      <Item
-        label="Password"
-        name="password"
-        rules={[validate.required(ValidateMessage.Password)]}
-      >
-        <Input.Password
-          value={password}
-          onChange={(evt) => setPassword(evt.target.value)}
-        />
-      </Item>
-
-      <Item className="form-login__button_wrapper">
-        <Button
-          type="primary"
-          htmlType="submit"
-          className="form-login__button"
-          loading={isLoading}
+        <Item
+          label="Username"
+          name="username"
+          rules={[validate.required(ValidateMessage.UserName)]}
         >
-          Submit
-        </Button>
-      </Item>
-    </Form>
+          <Input
+            value={user}
+            onChange={(evt) => setUser(evt.target.value)}
+            placeholder="foo"
+          />
+        </Item>
+
+        <Item
+          label="Password"
+          name="password"
+          rules={[validate.required(ValidateMessage.Password)]}
+        >
+          <Input.Password
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
+          />
+        </Item>
+
+        <Item className="form-login__button_wrapper">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="form-login__button"
+            loading={isLoading}
+          >
+            Submit
+          </Button>
+        </Item>
+      </Form>
+    </Card>
   );
 }
 
