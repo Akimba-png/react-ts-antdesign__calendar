91e20328a0b7b447a8e379896687faf657ef19da
Oleg <akimba@gmail.com>
2022-10-13 16:22:54 +0300

    style: Декомпозирует стилизацию NavMenu

--- src/pages/loading-screen/loading-screen.tsx
+++ src/pages/loading-screen/loading-screen.tsx
@@ -3,7 +3,7 @@ import './loading-screen.style.css';
 
 function LoadingScreen(): JSX.Element {
   return (
-    <Layout className="container loading-screen">
+    <Layout className="loading-screen">
       <Typography.Title className="loading-screen__title" level={2}>Loading...</Typography.Title>
       <Typography.Text className="loading-screen__text">please wait</Typography.Text>
       <Spin size="large"/>
